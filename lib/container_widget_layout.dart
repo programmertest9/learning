50338f96fe89caffbe6fd9b878580dde42fabf7d
programmertest9 <programmer.test.th@gmail>
2022-07-01 15:32:23 +0700

    flutter layout

--- lib/container_widget_layout.dart
+++ lib/container_widget_layout.dart
@@ -15,21 +15,49 @@ class _ContainerWidgetState extends State<ContainerWidget> {
         title: Text('CEnter widget'),
       ),
       body: Center(
-        child: Container(
-          width: 300.0,
-          height: 300.0,
-          // width: double.infinity,
-          // height: double.infinity,
-          // transform: Matrix4.rotationZ(45),
-          decoration: BoxDecoration(
-            color: Color(0xFF3be2a5),
-            // border: Border.all(width: 5, color: Colors.brown),
-            borderRadius: BorderRadius.circular(20),
-          ),
-          child: Text(
-            'This is my content',
-            textAlign: TextAlign.center,
-          ),
+        // child: Container(
+        //   width: 300.0,
+        //   height: 300.0,
+        //   // width: double.infinity,
+        //   // height: double.infinity,
+        //   // transform: Matrix4.rotationZ(45),
+        //   decoration: BoxDecoration(
+        //       color: Color(0xFF3be2a5),
+        //       // border: Border.all(width: 5, color: Colors.brown),
+        //       // borderRadius: BorderRadius.circular(20),
+        //       shape: BoxShape.circle,
+        //       image: DecorationImage(
+        //         image: NetworkImage(
+        //           "https://media.gettyimages.com/photos/leadership-concept-with-paper-airplanes-picture-id1132930101?s=612x612",
+        //         ),
+        //         fit: BoxFit.cover,
+        //       ),
+        //       boxShadow: [
+        //         BoxShadow(blurRadius: 10),
+        //       ]),
+        //   // child: Text(
+        //   //   'This is my content',
+        //   //   textAlign: TextAlign.center,
+        //   // ),
+        // ),
+
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Container(
+              // width: 300.0,
+              // height: 300.0,
+              width: double.infinity,
+              height: double.infinity,
+              // transform: Matrix4.rotationZ(45),
+              decoration: BoxDecoration(
+                color: Color(0xFF3be2a5),
+                // border: Border.all(width: 5, color: Colors.brown),
+                borderRadius: BorderRadius.circular(20),
+                gradient: LinearGradient(colors: [
+                  Color(0xFFB6F492),
+                  Color(0xFF338B93),
+                ]),
+              )),
         ),
       ),
     );
